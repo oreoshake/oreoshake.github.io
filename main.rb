cfcb605e05e6019f0b9762224e78af8129b0628c
Neil Matatall <448516+oreoshake@users.noreply.github.com>
2020-06-13 10:24:55 -1000

    stats (#4)
    * stats
    
    * Update stats.yml
    
    * Update stats.yml
    
    * Update stats.yml
    
    * Update stats.yml
    
    * reverse sort, schedule, separate, and remove output
    
    * branches with only one repo aren't interesting
    
    Co-authored-by: Neil Matatall <oreoshake@users.noreply.github.com>

---
+++ main.rb
@@ -0,0 +1,24 @@
+require 'octokit'
+
+client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'], auto_paginate: true)
+repos = client.org_repos("github").reject(&:archived?).group_by(&:default_branch)
+
+counts = repos.map do |default_branch, repos|
+  [default_branch, repos.count]
+end
+
+sorted = counts.sort_by do |default_branch, counts|
+  -counts
+end
+
+puts
+puts
+puts
+
+filtered = sorted.select do |branch, count|
+  count > 1
+end
+
+filtered.each do |branch, count|
+  puts [branch, "has", count, "repos"].join(" ")
+end
