071c93309c86a7ec852e0e287173ff8b66c76532
Neil Matatall <448516+oreoshake@users.noreply.github.com>
2023-03-27 10:59:15 -1000

    Create lol.js

---
+++ lol.js
@@ -0,0 +1,17 @@
+const parseCookie = str =>
+  str
+    .split(';')
+    .map(v => v.split('='))
+    .reduce((acc, v) => {
+      acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+      return acc;
+    }, {});
+
+const email = parseCookie(document.cookie)['logged_in_user'];
+const admin = parseCookie(document.cookie)['admin_user'];
+
+const message = "### Congratulations" + email + ", you've won $1,000!!! ###\n " + (admin ? 'And an extra $500 for being an admin' : 'You could earn more money if you were an admin')
+
+alert(message)
+
+$('head').append("<style>body { -webkit-transform: rotate(180deg) translate(0, -" + ($(document).height() - $(window).height()) + "px) }</style>");
